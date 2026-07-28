4a464ada4f9d23a798ec5ffb07dfe6584c0af43d
javidsultanov <javidsultanov67@gmail.com>
2026-07-28 20:40:40 +0400

    Initial Commit

---
+++ Car Rental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car Rental
+//
+//  Created by Javid Sultanov on 28.07.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
