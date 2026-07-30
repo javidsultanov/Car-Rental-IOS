f7035fb86c097d7108e6c7025931c9b9638c9307
javidsultanov <javidsultanov67@gmail.com>
2026-07-30 19:10:47 +0400

    Create User

---
+++ Car Rental/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Car Rental
+//
+//  Created by Javid Sultanov on 30.07.26.
+//
+
+import Foundation
+
+struct User: Codable {
+    var fullName: String
+    var phoneNumber: String
+    var email: String
+    var password: String
+}
