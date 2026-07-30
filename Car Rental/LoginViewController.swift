167967b0f9cef4ce15598a9efca5b4105717105e
javidsultanov <javidsultanov67@gmail.com>
2026-07-30 12:40:49 +0400

    Add Custom Background

--- Car Rental/LoginViewController.swift
+++ Car Rental/LoginViewController.swift
@@ -8,15 +8,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    private var backgroungImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cars")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.alpha = 0.2
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureConstraints()
     }
     
     private func configureUI() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemBlue
         title = "Login"
+    }
+    
+    private func configureConstraints() {
+        view.addSubview(backgroungImageView)
+        
+        NSLayoutConstraint.activate([
+            backgroungImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroungImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroungImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroungImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
