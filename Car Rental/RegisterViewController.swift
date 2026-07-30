141037bb8e9660a2e52cfda69c9134f05e75bb48
javidsultanov <javidsultanov67@gmail.com>
2026-07-30 18:00:22 +0400

    Create RegisterViewController

---
+++ Car Rental/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.swift
+//  Car Rental
+//
+//  Created by Javid Sultanov on 30.07.26.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        title = "Register"
+    }
+}
