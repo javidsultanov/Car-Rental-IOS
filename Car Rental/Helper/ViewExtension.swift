a359f74983f2b8db7f9a4e0ac113676f2d2f74f9
javidsultanov <javidsultanov67@gmail.com>
2026-07-30 19:18:14 +0400

    Add showDefaultAlert In Helper Folder

--- Car Rental/Helper/ViewExtension.swift
+++ Car Rental/Helper/ViewExtension.swift
@@ -6,3 +6,14 @@
 //
 
 import UIKit
+
+extension UIViewController {
+    func showDefaultAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default))
+        self.present(alert, animated: true)
+    }
+}
