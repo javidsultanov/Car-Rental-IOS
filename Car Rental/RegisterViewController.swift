d6d6f86f814f7aa3dde43efd4f10b0e2dfd24877
javidsultanov <javidsultanov67@gmail.com>
2026-07-30 18:14:40 +0400

    Add Custom Background In Register

--- Car Rental/RegisterViewController.swift
+++ Car Rental/RegisterViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    private var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cars")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.alpha = 0.2
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureConstraints()
     }
     
     private func configureUI() {
         title = "Register"
+        view.backgroundColor = .systemBlue
+    }
+    
+    private func configureConstraints() {
+        view.addSubview(backgroundImageView)
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
