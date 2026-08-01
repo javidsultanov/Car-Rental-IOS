c85220c93eadcc4b4be83d11229616aed3ef70f1
javidsultanov <javidsultanov67@gmail.com>
2026-08-01 06:38:48 +0400

    Add searchBar In Vehicles And Search

--- Car Rental/SearchController.swift
+++ Car Rental/SearchController.swift
@@ -8,15 +8,33 @@
 import UIKit
 
 class SearchController: UIViewController {
+    private var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "Search for a car"
+        bar.searchBarStyle = .minimal
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureConstraints()
     }
     
     private func configureUI() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .secondarySystemBackground
         navigationItem.title = "Search"
+    }
+    
+    private func configureConstraints() {
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            searchBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.92)
+        ])
     }
 }
--- Car Rental/VehiclesController.swift
+++ Car Rental/VehiclesController.swift
@@ -8,17 +8,36 @@
 import UIKit
 
 class VehiclesController: UIViewController {
+    private var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "Search for a car"
+        bar.searchBarStyle = .minimal
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
+    
     private var fileManager = CarFileManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureConstraints()
         fileManager.getCarItems()
     }
     
     private func configureUI() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .secondarySystemBackground
         navigationItem.title = "Car Rental"
+    }
+    
+    private func configureConstraints() {
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            searchBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.92)
+        ])
     }
 }
