410e4223d96f9db740e2f8373d67cdb70406dd6c
javidsultanov <javidsultanov67@gmail.com>
2026-08-01 05:45:02 +0400

    Create TabBar

---
+++ Car Rental/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  Car Rental
+//
+//  Created by Javid Sultanov on 01.08.26.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
